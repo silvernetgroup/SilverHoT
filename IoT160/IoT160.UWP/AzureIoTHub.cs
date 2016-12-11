3e18279d76c95c3c0f141ad5566a323124ec5030
Jarek Boguta <jarek.boguta92@gmail.com>
2016-12-11 08:07:53 +0100

    Table Reading

---
+++ IoT160/IoT160.UWP/AzureIoTHub.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+static class AzureIoTHub
+{
+    //
+    // Note: this connection string is specific to the device "SilverRPI". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "HostName=SilverHoTH.azure-devices.net;DeviceId=SilverRPI;SharedAccessKey=V0koXVTKMZ5cMcaO75pAXHo8MujIsdTYLMfEooBQOs0=";
+
+    //
+    // To monitor messages sent to device "SilverRPI" use iothub-explorer as follows:
+    //    iothub-explorer HostName=SilverHoTH.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=hEgI5+kDvERHFcrXW1xFoFkR4pK3K0h6/giRp6WkwBM= monitor-events "SilverRPI"
+    //
+
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+#if WINDOWS_UWP
+        var str = "Hello, Cloud from a UWP C# app!";
+#else
+        var str = "Hello, Cloud from a C# app!";
+#endif
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
--- IoT160/IoT160.UWP/MainPage.xaml.cs
+++ IoT160/IoT160.UWP/MainPage.xaml.cs
@@ -14,7 +14,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
-
+using Microsoft.WindowsAzure.Storage.Auth;
+using System.Diagnostics;
 
 namespace IoT160.UWP
 {
@@ -25,6 +26,127 @@ namespace IoT160.UWP
             this.InitializeComponent();
 
             LoadApplication(new IoT160.App());
+
+            GetTableData();
         }
+
+        public async void GetTableData()
+        {
+            string partitionKey = "SilverRPI";
+            string rowKey = "1";
+
+            //StorageUri uri = new StorageUri(https://silverhot.table.core.windows.net/RPIData);
+            try
+            {
+                var accountName = "silverhot";
+                var accountKey = "+aR0rcYc4pF9WfhTKmayejycGGRx4tqDp7/LN95BYg5wsIP+8XCL6r0SPFSE6TNEWAi9WEHdK/2O02JYDQiCRg==";
+                var credentials = new StorageCredentials(accountName, accountKey);
+                var account = new CloudStorageAccount(credentials, true);
+
+                CloudTableClient client = account.CreateCloudTableClient();
+                CloudTable table = client.GetTableReference("RPIData");
+
+                //TableOperation retrieveOperation = TableOperation.Retrieve<Entity>(partitionKey, rowKey);
+
+                //TableOperation op = TableOperation.Retrieve<Entity>(partitionKey, rowKey);
+                //var res = await table.ExecuteAsync(op);
+                //Entity e = (Entity)res.Result;
+
+                TableQuery<Entity> query = new TableQuery<Entity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "SilverRPI"));
+
+                //TableQuery<Entity> query = new TableQuery<Entity>();//.Where(TableQuery.GenerateFilterCondition(partitionKey,QueryComparisons.Equal,"SilverRPI"));
+                //Debug.WriteLine(((Entity)query.Result).PartitionKey);
+                //Debug.WriteLine(((Entity)query.Result).RowKey);
+                /*Debug.WriteLine(e.PartitionKey);
+                Debug.WriteLine(e.RowKey);
+                Debug.WriteLine(e.deviceid);
+                Debug.WriteLine(e.message);
+                Debug.WriteLine(e.ToString());*/
+
+                //var temp = await table.ExecuteAsync(query);
+
+                /*foreach (Entity item in temp)
+                {
+                    Debug.WriteLine("{0},{1},{2},{3},{4},{5}", item.PartitionKey, item.RowKey, item.deviceid, item.message, item.time);
+                }*/
+
+                /*if (query.Result != null)
+                {
+                    outline = outline + ((ServiceAlertsEntity) query.Result).alertMessage + " * ";
+                }
+                else
+                {
+                    Console.WriteLine("No Alerts");
+                }*/
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        public class Entity : TableEntity
+        {
+            public Entity(string pk, string rk,string msg)
+            {
+                this.PartitionKey = pk;
+                this.RowKey = rk;
+                this.message = msg;
+            }
+            public Entity() { }
+            public string deviceid { get; set; }
+            public string message { get; set; }
+            public string time { get; set; }
+        }
+
+
+        /*public class Entity:ITableEntity
+        {
+            public string ETag
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string PartitionKey
+            {
+                get; set;
+            }
+
+            public string RowKey
+            {
+                get; set;
+            }
+
+            public DateTimeOffset Timestamp
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+            {
+                throw new NotImplementedException();
+            }
+        }*/
     }
 }
